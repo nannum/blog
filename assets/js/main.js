108053c39dfcbec6846bd69e814632bd22fab7ff
Niilaryea <niilaaye@gmail.com>
2020-12-09 14:25:00 -0500

    refactor: Update the js
    - needed to accomidate pages without a top skip link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 (() => {
-//Ends and triggers animations when the top of the page is in view. 
+//Hides and shows page skip links when the top of the page is in view.
+	const articleHeader = document.querySelector('.header--article');
 	const skipLinkTop = document.querySelector('.skip-link--top');
 	const skipLinkBottom = document.querySelector('.skip-link--bottom');
 	const config = {
@@ -7,7 +8,7 @@
 		rootMargin: '125px 0px 0px 0px',
 		threshold: 0
 	};
-
+	let observedItem = skipLinkTop ? skipLinkTop : articleHeader;
 	const observer = new IntersectionObserver(entries => {
 		entries.forEach(entry => {
 			//TODO: remove debug console log
@@ -15,7 +16,7 @@
 			if (entry.intersectionRatio > 0) {
 				//TODO: remove debug console log
 				console.log("on screen");
-				//TODO: show back to top link
+				//Show back to top link
 				if (skipLinkBottom.classList.contains('slide-in')) {
 					skipLinkBottom.classList.remove('slide-in');
 					skipLinkBottom.classList.add('slide-out');
@@ -23,12 +24,11 @@
 			} else {
 				//TODO: remove debug console log
 				console.log("off screen");
-				//TODO: remove back to top link
+				//Hide back to top link
 				skipLinkBottom.classList.add('slide-in');
 				skipLinkBottom.classList.remove('slide-out');
 			}
 		});
 	}, config);
-
-	observer.observe(skipLinkTop);
+	observer.observe(observedItem);
 })();
