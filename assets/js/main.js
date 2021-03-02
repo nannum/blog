b1f7a2a7a16fb4d30400a721277b44ab70ce35f0
Niilaryea <niilaaye@gmail.com>
2021-03-02 15:27:24 -0500

    refactor: Correct bug preventing skip link from appearing on article pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,25 @@
 (() => {
-//Hides and shows page skip links when the top of the page is in view.
-	const articleHeader = document.querySelector('.header--article');
-	const skipLinkTop = document.querySelector('.skip-link--top');
-	const skipLinkBottom = document.querySelector('.skip-link--bottom');
+//Hides the bottom content skip link when the page header enters the device's viewpoint 
+//Reveals the bottom content skip link when the page header exits the device's viewpoint
+	const skipLink = document.querySelector('.skip-link--bottom');
 	const config = {
 		root: null,
 		rootMargin: '125px 0px 0px 0px',
 		threshold: 0
 	};
-	let observedItem = skipLinkTop ? skipLinkTop : articleHeader;
+	let observedItem = document.querySelector('.header');
 	const observer = new IntersectionObserver(entries => {
 		entries.forEach(entry => {
-			//TODO: remove debug console log
-			console.log(`entry.intersectionRatio= ${entry.intersectionRatio}`);
 			if (entry.intersectionRatio > 0) {
-				//TODO: remove debug console log
-				console.log("on screen");
 				//Show back to top link
-				if (skipLinkBottom.classList.contains('slide-in')) {
-					skipLinkBottom.classList.remove('slide-in');
-					skipLinkBottom.classList.add('slide-out');
+				if (skipLink.classList.contains('slide-in')) {
+					skipLink.classList.remove('slide-in');
+					skipLink.classList.add('slide-out');
 				}
 			} else {
-				//TODO: remove debug console log
-				console.log("off screen");
 				//Hide back to top link
-				skipLinkBottom.classList.add('slide-in');
-				skipLinkBottom.classList.remove('slide-out');
+				skipLink.classList.add('slide-in');
+				skipLink.classList.remove('slide-out');
 			}
 		});
 	}, config);
